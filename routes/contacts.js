961b81d4ab1b0646c2034125159341766b1c1c68
LawrenceYamete <LawrenceYamete>
2023-05-04 10:20:21 +0800

    changes

--- routes/contacts.js
+++ routes/contacts.js
@@ -7,25 +7,4 @@ router.get('/', contactsController.getAll);
 
 router.get('/:id', contactsController.getSingle);
 
-module.exports = router;    
-
-
-const MongoClient = require('mongodb').MongoClient;
-
-const uri = "mongodb+srv://<username>:<password>@<cluster>.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-
-client.connect(err => {
-  const collection = client.db("test").collection("users");
-
-  // Update all documents with age less than 30 to have age 30
-  collection.updateMany(
-    { age: { $lt: 30 } },
-    { $set: { age: 30 } },
-    (err, res) => {
-      if (err) throw err;
-      console.log(`${res.result.nModified} documents updated`);
-      client.close();
-    }
-  );
-});
+module.exports = router; 
